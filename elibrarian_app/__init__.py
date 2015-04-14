3f22e8f13a7ee7e1819daccd57a1e8e20f5c7450
Oleksandr Poliatykin <poliatykin@gmail.com>
2015-04-14 23:40:57 +0300

    fix: removed flask-restful from dependencies

--- elibrarian_app/__init__.py
+++ elibrarian_app/__init__.py
@@ -1,10 +1,8 @@
 from config import config
 from flask import Flask
 from flask.ext.bootstrap import Bootstrap
-from flask.ext.restful import Api
 from flask.ext.sqlalchemy import SQLAlchemy
 
-api = Api()
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 
@@ -14,7 +12,6 @@ def create_app(config_name):
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
-    api.init_app(app)
     bootstrap.init_app(app)
     db.init_app(app)
 
